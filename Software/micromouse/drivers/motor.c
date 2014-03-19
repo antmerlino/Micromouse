0692b7b3bf2f097e5db79fe77196ec5d9fe2c130
Anthony Merlino <merlin17@students.rowan.edu>
2014-03-18 21:47:01 -0400

    Creates TI-RTOS project for micromouse software

---
+++ Software/micromouse/drivers/motor.c
@@ -0,0 +1,10 @@
+/*
+ * motor.c
+ *
+ *  Created on: Mar 18, 2014
+ *      Author: Anthony Merlino and Brad Ebinger
+ */
+
+
+#include "motor.h"
+
---
+++ Software/micromouse/drivers/motor.h
@@ -0,0 +1,14 @@
+/*
+ * motor.h
+ *
+ *  Created on: Mar 18, 2014
+ *      Author: Anthony
+ */
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+void motors_init();
+
+
+#endif /* MOTOR_H_ */
